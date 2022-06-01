b99d5580ffb2e336c57bf03673f5c7f3e32c3bb6
Javier <Javier@DESKTOP-B6ROK0N>
2022-06-01 15:27:15 -0500

    Agregar archivos de proyecto.

---
+++ University.App/University.App/DTOs/LoginDTO.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace University.App.DTOs
+{
+    public class LoginReqDTO
+    {
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("password")]
+        public string Password { get; set; }
+    }
+
+    public class LoginResDTO
+    {
+        [JsonProperty("token")]
+        public string Token { get; set; }
+    }
+
+    public class LoginResFailDTO
+    {
+        [JsonProperty("error")]
+        public string Error { get; set; }
+    }
+}
